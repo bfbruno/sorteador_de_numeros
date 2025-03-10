0379b001b0509391a3dab1797294671158d2a377
Bruno <brunobeckhauserfagundes@outlook.com>
2025-03-09 21:49:55 -0300

    second commit

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ function sortear() {
     let ate = parseInt(document.getElementById("ate").value);
     let sorteados = [];
     let numero;
-    if (de >= ate) {
-        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
-        return;
-    }
+
     if (de > ate) {
         document.getElementById("quantidade").value = "";
         document.getElementById("de").value = "";
         document.getElementById("ate").value = "";
         document.getElementById("resultado").innerHTML = "<label class='texto__paragrafo'>Números sorteados:  nenhum até agora</label>";
         alert("Do número está maior que o Até o número");
-    }else if (quant > ate) {
+    }else if (quant > (ate - de + 1)) {
         document.getElementById("quantidade").value = "";
         document.getElementById("de").value = "";
         document.getElementById("ate").value = "";
